cf947548bdb26096bf6f96dcc0ea4afa2897f5fb
rbrtsmith <rbrtsmith84@googlemail.com>
2017-07-24 15:41:54 +0100

    setup config

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = {
+  extends: "stylelint-config-sass-guidelines",
+  plugins: [
+    "stylelint-scss",
+    "stylelint-selector-bem-pattern",
+  ],
+  rules: {
+    'selector-class-pattern': "",
+    'order/properties-alphabetical-order': null,
+    'selector-no-qualifying-type': [true, {
+      ignore: ["attribute"],
+    }],
+    'scss/selector-no-redundant-nesting-selector': true,
+    'scss/at-extend-no-missing-placeholder': true,
+    'scss/dollar-variable-no-missing-interpolation': true,
+    'scss/at-mixin-argumentless-call-parentheses': 'never',
+    'plugin/selector-bem-pattern': {
+      'preset': 'bem',
+      'componentName': '(([a-z0-9]+(?!-$)-?)+)',
+      componentSelectors: {
+        initial: "\\.{componentName}(((__|--)(([a-z0-9\\[\\]'=]+(?!-$)-?)+))+)?$",
+      },
+    },
+  },
+};
